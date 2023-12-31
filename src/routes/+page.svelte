22d33c0aeaa79c7d9d03e13ad47f3c3c79635973
Mugilan <mugilsubramani@gmail.com>
2024-01-01 00:08:02 +0530

    redesigned the website in svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,58 @@
+<div class="flex flex-col max-w-screen-md justify-center mx-auto font-inter">
+    <div class="m-4 fixed bottom-12 text-sm md:text-base md:top-4 z-10">
+      <ul class="navbar flex flex-row fixed left-1/2 transform -translate-x-1/2 bg-opacity-80 gap-4 md:gap-6 bg-[#fefffe] backdrop-blur-lg border border-gray-200 border-opacity-60 md:border-opacity-80 w-min p-3 px-6 rounded-full">
+        <li> <a href="/">Home</a> </li>
+        <li> <a href="/about">About</a> </li>
+        <li> <a href="/work">Work</a> </li>
+        <li> <a href="/books">Books</a> </li>
+        <li> <a href="/contact">Contact</a> </li>
+      </ul>
+    </div>
+   
+    <!--HEADER   
+    <div class="header ">
+        <ul class="flex flex-row justify-between m-4">
+            <li><p class="font-bold">Mugilan</p></li>
+            <li>
+                <div class="flex gap-1">
+
+                </div>
+            </li>
+        </ul>
+    </div> -->
+  
+
+    <!--HERO-->
+    <div class="mt-8 md:mt-24 mx-4 md:mx-8 lg:mx-0 font-satoshi">
+      <h1 class="text-h3 md:text-h2 lg:text-h1 font-bold ">Mugilan Subramaniyan ✨</h1>
+      <h2 class="text-p lg:text-h6 font-medium text-[#666666]">Web Developer and Designer</h2>
+      <p class="text-small lg:text-p font-medium py-4">Hi, my name is Mugilan, I am a web developer and designer from India.  Currently open for freelance collaborations.</p>
+    </div>
+
+
+    <!--WORKS-->
+    <div class="mx-4 md:mx-8 lg:mx-0 mt-2 md:mt-6 grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div class="flex flex-col justify-between rounded-lg min-w-full bg-[#fefffe]  h-full min-h-64 md:min-h-72 w-8/12 backdrop-blur-lg border border-gray-200 border-opacity-60">
+            <div><p class="mx-4 py-3 text-sm font-medium font-inter text-[#666]">Mahathaan</p></div>
+            <div><p class="mx-4 py-3 text-sm font-regular text-[#666]">Mahathaan</p></div>
+        </div>
+        <div class="flex flex-col justify-between rounded-lg min-w-full bg-[#fefffe]  h-full min-h-64 md:min-h-72 w-8/12 backdrop-blur-lg border border-gray-200 border-opacity-60">
+            <div><p class="mx-4 py-3 text-sm font-medium font-inter text-[#666]">Mahathaan</p></div>
+            <div><p class="mx-4 py-3 text-sm font-regular text-[#666]">Mahathaan</p></div>
+        </div>
+        <div class="flex flex-col justify-between rounded-lg min-w-full bg-[#fefffe]  h-full min-h-64 md:min-h-72 w-8/12 backdrop-blur-lg border border-gray-200 border-opacity-60">
+            <div><p class="mx-4 py-3 text-sm font-medium font-inter text-[#666]">Mahathaan</p></div>
+            <div><p class="mx-4 py-3 text-sm font-regular text-[#666]">Mahathaan</p></div>
+        </div>
+        <div class="flex flex-col justify-between rounded-lg min-w-full bg-[#fefffe]  h-full min-h-64 md:min-h-72 w-8/12 backdrop-blur-lg border border-gray-200 border-opacity-60">
+            <div><p class="mx-4 py-3 text-sm font-medium font-inter text-[#666]">Mahathaan</p></div>
+            <div><p class="mx-4 py-3 text-sm font-regular text-[#666]">Mahathaan</p></div>
+        </div>
+        <div class="flex flex-col justify-between rounded-lg min-w-full bg-[#fefffe]  h-full min-h-64 md:min-h-72 w-8/12 backdrop-blur-lg border border-gray-200 border-opacity-60">
+            <div><p class="mx-4 py-3 text-sm font-medium font-inter text-[#666]">Mahathaan</p></div>
+            <div><p class="mx-4 py-3 text-sm font-regular text-[#666]">Mahathaan</p></div>
+        </div>
+
+    </div>
+
+  </div>
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+        'satoshi': ['Satoshi', 'sans-serif'],
+      },
+
+      fontSize : {
+        'h1': '2.986rem',
+        'h2': '2.488rem',
+        'h3': '2.074rem',
+        'h4': '1.728rem',
+        'h5': '1.44rem',
+        'h6': '1.2rem',
+        'p': '1rem',
+        'small': '0.833rem',
+        'xsmall': '0.694rem'
+      },
+    }
+
+
+  },
+  plugins: [
+    require('tailwindcss-filters'),
+  ]
+};
