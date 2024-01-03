7be0af430cf821233dc7a9df89910efc4f0c2925
Mugilan <86902446+NotMugil@users.noreply.github.com>
2024-01-03 12:51:48 +0530

    Update bookgrid.svelte

--- src/lib/components/bookgrid/bookgrid.svelte
+++ src/lib/components/bookgrid/bookgrid.svelte
@@ -5,7 +5,7 @@
 <div class="grid grid-cols-1 md:grid-cols-3 gap-4"> 
     {#each books as { title, author, cover } (title)}
         <div class="p-4 rounded-lg backdrop-blur-lg border border-gray-200 border-opacity-60 bg-[#fefefe] font-satoshi">
-            <div class="w-full h-80 md:h-72 rounded-lg overflow-hidden mb-2">
+            <div class="w-full h-96 md:h-72 rounded-lg overflow-hidden mb-2">
                 <img src={cover} alt={title} class="object-cover w-full h-full  rounded-lg shadow-sm" loading="lazy"/>
             </div>
             <p class="text-lg font-bold">{title}</p>
