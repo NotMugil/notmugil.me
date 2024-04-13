650166f0580941675d5c4826a4383f04b7a5dd35
NotMugil <mugilsubramani@gmail.com>
2024-04-13 16:23:08 +0530

    minor changes

--- src/lib/components/aboutgrid.svelte
+++ src/lib/components/aboutgrid.svelte
@@ -1,85 +1,127 @@
-<div class="mt-8 mx-4 md:mx-8 lg:mx-0 grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-12 auto-rows-fr text-center ">
-    
-    <div class="flex flex-col">
-        <!--Intro text-->
-        <div class="bg-[#0e0e0e] rounded-lg lg:row-span-1 order-1 flex-col flex mb-8 gap-4">
-            <p class="text-p text-left lg:text-p font-normal text-[#f2f2f2] md:max-w-[90%] cursor-default">
-                I'm Mugilan, who designs and develops ideas into interfaces. Specialized in Full-Stack development and UI/UX Design.
-            </p>
-            <p class="text-p text-left lg:text-p font-normal text-[#f2f2f2] md:max-w-[90%] cursor-default">
-                Growing up in Tamil Nadu, a culture that values honesty and teamwork, shaped me into a team player who is open and understands the lives of ordinary people.
-            </p>
-        </div>
-        <!--Perfil-->
-        <div class="order-4 border border-white border-opacity-10 bg-[#0e0e0e] hover:drop-shadow rounded-lg p-2 border-2 border-white border-opacity-10 lg:row-span-1 lg:col-span-1">
-            <img src="https://res.cloudinary.com/dimb2aadu/image/upload/v1713002448/IMG_7060_owekvb.jpg" alt="" class="rounded-lg border border-white border-opacity-10 hover:border-opacity-50 filter hue-rotate saturate">
-        </div>
+<div
+	class="mt-8 mx-4 md:mx-8 lg:mx-0 grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-12 auto-rows-fr text-center"
+>
+	<div class="flex flex-col">
+		<!--Intro text-->
+		<div class="bg-[#0e0e0e] rounded-lg lg:row-span-1 order-1 flex-col flex mb-8 gap-4">
+			<p
+				class="text-p text-left lg:text-p font-normal text-[#f2f2f2] md:max-w-[90%] cursor-default"
+			>
+				I'm Mugilan, who designs and develops ideas into interfaces. Specialized in Full-Stack
+				development and UI/UX Design.
+			</p>
+			<p
+				class="text-p text-left lg:text-p font-normal text-[#f2f2f2] md:max-w-[90%] cursor-default"
+			>
+				Growing up in Tamil Nadu, a culture that values honesty and teamwork, shaped me into a team
+				player who is open and understands the lives of ordinary people.
+			</p>
+		</div>
+		<!--Perfil-->
+		<div
+			class="order-4 border border-white border-opacity-10 bg-[#0e0e0e] hover:drop-shadow rounded-lg p-2 border-2 border-white border-opacity-10 lg:row-span-1 lg:col-span-1"
+		>
+			<img
+				src="https://res.cloudinary.com/dimb2aadu/image/upload/v1713002448/IMG_7060_owekvb.jpg"
+				loading="eager"
+				alt="perfil image of me"
+				class="rounded-lg border border-white border-opacity-10 hover:border-opacity-50 filter hue-rotate saturate"
+			/>
+		</div>
+	</div>
 
-    </div>
+	<div class="flex flex-col gap-4">
+		<div
+			class="order-4 md:order-2 border border-white border-opacity-10 bg-[#0e0e0e] hover:drop-shadow rounded-lg p-2 border-2 border-white border-opacity-10 lg:row-span-1 lg:col-span-1"
+		>
+			<img
+				src="https://res.cloudinary.com/dimb2aadu/image/upload/v1713002623/IMG_2058_bmixte.jpg"
+				loading="eager"
+				alt="image of a messy book stall"
+				class="rounded-lg filter border border-white border-opacity-10 hover:border-opacity-50 hue-rotate saturate"
+			/>
+		</div>
 
-    <div class="flex flex-col gap-4">
-
-        <div class="order-4 md:order-2 border border-white border-opacity-10 bg-[#0e0e0e] hover:drop-shadow rounded-lg p-2 border-2 border-white border-opacity-10 lg:row-span-1 lg:col-span-1">
-            <img src="https://res.cloudinary.com/dimb2aadu/image/upload/v1713002623/IMG_2058_bmixte.jpg" alt="" class="rounded-lg filter border border-white border-opacity-10 hover:border-opacity-50 hue-rotate saturate">
-        </div>
-
-        <div class="bg-[#0e0e0e] rounded-lg order-2 md:order-3 lg:row-span-1 flex-col text-left">
-            <h1 class="text-md md:text-sm font-inter font-normal cursor-default text-[#666666]">THINGS I LOVE TO DO</h1>
-        </div>
-        <div class="order-3 md:order-4 text-left"> 
-            <h2 class="text-p lg:text-h6 font-regular  cursor-default">Visiting aesthetic cafes</h2>
-            <h2 class="text-p lg:text-h6 font-regular  cursor-default">Playing story games</h2>
-            <h2 class="text-p lg:text-h6 font-regular  cursor-default">Finding book stalls and buying a awful lot of books that i will never read.</h2>
-        </div>
-
-        
-    </div>
-
+		<div class="bg-[#0e0e0e] rounded-lg order-2 md:order-3 lg:row-span-1 flex-col text-left">
+			<h1 class="text-md md:text-sm font-inter font-normal cursor-default text-[#666666]">
+				THINGS I LOVE TO DO
+			</h1>
+		</div>
+		<div class="order-3 md:order-4 text-left">
+			<h2 class="text-p lg:text-h6 font-regular cursor-default">Visiting aesthetic cafes</h2>
+			<h2 class="text-p lg:text-h6 font-regular cursor-default">Playing story games</h2>
+			<h2 class="text-p lg:text-h6 font-regular cursor-default">
+				Finding book stalls and buying a awful lot of books that i will never read.
+			</h2>
+		</div>
+	</div>
 </div>
 
-<div class="mt-12 mx-4 md:mx-8 lg:mx-0 grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-12 auto-rows-fr text-center ">
-
-    <div class="flex flex-col gap-4">
-
-        <div class="bg-[#0e0e0e] rounded-lg lg:row-span-1 flex-col text-left">
-            <h1 class="text-md md:text-sm font-inter font-normal cursor-default text-[#666666]">IN MY SPARE TIME,</h1>
-        </div>
-
-        <div class="bg-[#0e0e0e] rounded-lg lg:row-span-1 md: flex-col flex mb-8 gap-4">
-            <p class="text-p text-left lg:text-p font-normal text-[#f2f2f2] md:max-w-[90%] cursor-default">
-                From 2022 to present, I've been dedicated to volunteering at various impactful organizations in Chennai.
-            </p>
-            <p class="text-p text-left lg:text-p font-normal text-[#f2f2f2] md:max-w-[90%] cursor-default">
-                My involvement with <span class="underline decoration-dotted hover:bg-[#666666] transition-all duration-300"><a href="https://fossunited.org/chennai">FOSS United Chennai</a></span> revolves around utilization and development of FOSS within the local community of Chennai.
-            </p>
-            <p class="text-p text-left lg:text-p font-normal text-[#f2f2f2] md:max-w-[90%] cursor-default">
-                In 2023, I was a volunteer at Devfest Chennai 2023, a yearly event organized by <span class="underline decoration-dotted hover:bg-[#666666] transition-all duration-300"><a href="https://gdgchennai.in/">Google Developer Groups.</a></span> 
-            </p>
-            <p class="text-p text-left lg:text-p font-normal text-[#f2f2f2] md:max-w-[90%] cursor-default">
-                Additionally, my participation in <span class="underline decoration-dotted hover:bg-[#666666] transition-all duration-300"><a href="https://www.joshtalks.com/">Josh Talks</a></span> from 2022 to 2023 allowed me to contribute to spreading inspiring narratives of diverse careers and role models, reaching audiences in multiple regional languages.
-            </p>
-        </div>
-
-    </div>
-
-    <div class="flex flex-col">
-    
-       
-        <!--Perfil-->
-        <div class="lg:row-span-1 lg:col-span-2 border border-white border-opacity-10 bg-[#0e0e0e] hover:drop-shadow rounded-lg p-2 border-2 border-white border-opacity-10 lg:row-span-1 lg:col-span-1">
-            <img src="https://res.cloudinary.com/dimb2aadu/image/upload/v1713003072/IMG_8577_ztekkb.jpg" alt="" class="rounded-lg border border-white border-opacity-10 hover:border-opacity-50 filter hue-rotate saturate">
-        </div>
-
-        
-
-    </div>
-
- 
-
-
+<div
+	class="mt-12 mx-4 md:mx-8 lg:mx-0 grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-12 auto-rows-fr text-center"
+>
+	<div class="flex flex-col gap-4">
+		<div class="bg-[#0e0e0e] rounded-lg lg:row-span-1 flex-col text-left">
+			<h1 class="text-md md:text-sm font-inter font-normal cursor-default text-[#666666]">
+				IN MY SPARE TIME,
+			</h1>
+		</div>
+		<div class="bg-[#0e0e0e] rounded-lg lg:row-span-1 md: flex-col flex mb-8 gap-4">
+			<p
+				class="text-p text-left lg:text-p font-normal text-[#f2f2f2] md:max-w-[90%] cursor-default"
+			>
+				From 2022 to present, I've been dedicated to volunteering at various impactful organizations
+				in Chennai.
+			</p>
+			<p
+				class="text-p text-left lg:text-p font-normal text-[#f2f2f2] md:max-w-[90%] cursor-default"
+			>
+				My involvement with <span
+					class="underline decoration-dotted hover:bg-[#666666] transition-all duration-300"
+					><a href="https://fossunited.org/chennai">FOSS United Chennai</a></span
+				> revolves around utilization and development of FOSS within the local community of Chennai.
+			</p>
+			<p
+				class="text-p text-left lg:text-p font-normal text-[#f2f2f2] md:max-w-[90%] cursor-default"
+			>
+				In 2023, I was a volunteer at Devfest Chennai 2023, a yearly event organized by <span
+					class="underline decoration-dotted hover:bg-[#666666] transition-all duration-300"
+					><a href="https://gdgchennai.in/">Google Developer Groups.</a></span
+				>
+			</p>
+			<p
+				class="text-p text-left lg:text-p font-normal text-[#f2f2f2] md:max-w-[90%] cursor-default"
+			>
+				Additionally, my participation in <span
+					class="underline decoration-dotted hover:bg-[#666666] transition-all duration-300"
+					><a href="https://www.joshtalks.com/">Josh Talks</a></span
+				> from 2022 to 2023 allowed me to contribute to spreading inspiring narratives of diverse careers
+				and role models, reaching audiences in multiple regional languages.
+			</p>
+		</div>
+	</div>
+	<div class="flex flex-col">
+		<!--Perfil-->
+		<div
+			class="lg:row-span-1 lg:col-span-2 border border-white border-opacity-10 bg-[#0e0e0e] hover:drop-shadow rounded-lg p-2 border-2 border-white border-opacity-10 lg:row-span-1 lg:col-span-1"
+		>
+			<img
+				src="https://res.cloudinary.com/dimb2aadu/image/upload/v1713003072/IMG_8577_ztekkb.jpg"
+				loading="eager"
+				alt="volunteering in gdg devfest 2023"
+				class="rounded-lg border border-white border-opacity-10 hover:border-opacity-50 filter hue-rotate saturate"
+			/>
+		</div>
+	</div>
 </div>
 
-
-<div class="mx-4 md:mx-8 lg:mx-0 mt-12 lg:row-span-1 border border-white border-opacity-10 bg-[#0e0e0e] hover:drop-shadow rounded-lg p-2 lg:col-span-1 lg:row-span-1 w-full h-min">
-    <img src="https://res.cloudinary.com/dimb2aadu/image/upload/v1712995348/IMG_7098_1_dvag31.jpg" alt="" class="rounded-lg w-full h-max border border-white border-opacity-10 hover:border-opacity-50">
+<div
+	class="mx-4 md:mx-8 lg:mx-0 mt-12 lg:row-span-1 border border-white border-opacity-10 bg-[#0e0e0e] hover:drop-shadow rounded-lg p-2 lg:col-span-1 lg:row-span-1 w-full h-min"
+>
+	<img
+		src="https://res.cloudinary.com/dimb2aadu/image/upload/v1712995348/IMG_7098_1_dvag31.jpg"
+		loading="eager"
+		alt="volunteering in gdg devfest 2023"
+		class="rounded-lg w-full h-max border border-white border-opacity-10 hover:border-opacity-50"
+	/>
 </div>
