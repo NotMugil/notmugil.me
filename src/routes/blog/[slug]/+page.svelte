ee1833c754e58bf31da408536b6b48badd1f7777
NotMugil <mugilsubramani@gmail.com>
2024-07-03 15:52:48 +0530

    refactor: change locale us to uk

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -2,8 +2,8 @@
     export let data;
 
     function formatDate(dateString: string) {
-        const options: Intl.DateTimeFormatOptions = { day: '2-digit', month: 'short', year: '2-digit' };
-        return new Date(dateString).toLocaleDateString('en-US', options);
+        const options: Intl.DateTimeFormatOptions = { day: '2-digit', month: 'short', year: 'numeric' };
+        return new Date(dateString).toLocaleDateString('en-UK', options);
     }
 
 </script>
